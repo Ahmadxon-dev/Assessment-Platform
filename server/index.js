dfc0f295599603b84ded1ed8734e07c2839cf5fb
Ahmadxon-dev <ahmadxon2008@gmail.com>
2026-04-08 23:57:25 +0500

    mongo uri change

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ async function start() {
         app.listen(PORT, () => {
             console.log(`Server has been started on port ${PORT}`);
         })
-        await mongoose.connect(process.env.MONGO_URI + "/test");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("DB connected successfully");
 
     } catch (error) {
